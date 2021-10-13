7c2965c19cb0585e12ecbcaf0973d3c4f7f7dd60
DESKTOP-0OMI4UN\ardian <yodhaardiansyah300@gmail.com>
2021-10-13 18:53:56 +0700

    Unity File & Standalone Add

---
+++ Assets/Script/TabArea.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TabArea : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
